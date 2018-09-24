c82991c7615de6cd331190e04821aeab3904132e
edpantzar@gmail.com <edpantzar@gmail.com>
2018-09-24 14:11:45 -0500

    Added requirements for dev env and bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHandshake, faClipboardList, faCodeBranch, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+function escapeLinter(arr) {
+  // return nothing
+  return;
+};
+
+// Prevents linter warnings about unused packages
+escapeLinter($, Popper, FontAwesomeIcon);
+
+
+library.add(faHandshake, faClipboardList, faCodeBranch, faEnvelope);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
