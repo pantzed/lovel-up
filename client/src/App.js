b6a97cf16ed6cb0dc6e0605208124692a90c54e1
Ed Pantzar <edpantzar@gmail.com>
2018-09-26 10:06:41 -0500

    Added socket server and chat functionality

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super(props);
     this.state = {
       nav: false,
-      login: true,
+      login: false,
       profile: false,
       matches: false,
       chat: false,
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,12 +1,38 @@
 import * as React from 'react';
 import './Chat.css';
+import io from 'socket.io-client';
+
+const socket=io.connect('http://localhost:8000')
 
 class Chat extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      messages: [],
+    };
   }
+
+  componentDidMount() {
+    // const socket = io();
+    let message = document.getElementById('m');
+    let submit = document.getElementById('submit');
+
+    submit.addEventListener("click", (event) => {
+      event.preventDefault();
+      socket.emit('chat message', message.value);
+      console.log(message.value);
+      message.value = '';
+    });
+  
+    socket.on('chat message', (msgs)=> {
+      this.setState({
+        messages: msgs,
+      });
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <div className='d-flex justify-content-end mt-2'>
@@ -22,11 +48,12 @@ class Chat extends React.Component {
             </div>
             <div className='row'>
               <div className='col-12'>
-                <ul className='chat-stream mt-4 text-light'>
+                <ul id="messagesContainer" className='chat-stream mt-4 text-light'>
                   <li className='py-3 pl-2 pr-5 my-1 align-self-start bg-primary rounded shadow-sm'>Hello!</li>
                   <li className='py-3 pl-2 pr-5 my-1 align-self-end bg-info rounded shadow-sm'>Hi!</li>
-                  <li className='py-3 pl-2 pr-5 my-1 align-self-start bg-primary rounded shadow-sm'>Let's date?</li>
-                  <li className='py-3 pl-2 pr-5 my-1 align-self-end bg-info rounded shadow-sm'>OK!</li>
+                  {this.state.messages.map((message, index)=>{
+                    return  <li key={index} className='py-3 pl-2 pr-5 my-1 align-self-start bg-primary rounded shadow-sm'>{message}</li>
+                  })}
                 </ul>
               </div>
             </div>
@@ -34,9 +61,9 @@ class Chat extends React.Component {
               <div className='col-12 mt-3'>
                 <form action='' className='form-inline'>
                   <div className='form-group m-0'>
-                    <input className="form-control" type="text" placeholder="Default input" />
+                    <input id='m' className="form-control" type="text" placeholder="Default input" />
                   </div>
-                  <button type='submit' className='btn btn-outline-primary ml-2'>Send</button>
+                  <button id='submit' type='submit' className='btn btn-outline-primary ml-2'>Send</button>
                 </form>
               </div>
             </div>
