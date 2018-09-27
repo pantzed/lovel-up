42c1fd77492ce566d0255b3a606728f14e00175a
edpantzar@gmail.com <edpantzar@gmail.com>
2018-09-27 16:36:38 -0500

    Navigation refactor

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -12,8 +12,8 @@ class Nav extends React.Component {
         <div className='row d-flex justify-content-center'>
           <div className='col-12 mt-5 d-flex flex-column'>
             <h1>Navigation</h1>
-            <button type='button' className='mt-2 btn btn-outline-primary' onClick={(e) => this.props.activatePage(e, 'PROFILE', 'NAV')}>Profile</button>
             <button type='button' className='mt-2 btn btn-outline-primary' onClick={(e) => this.props.activatePage(e, 'LOGIN', 'NAV')}>Login</button>
+            <button type='button' className='mt-2 btn btn-outline-primary' onClick={(e) => this.props.activatePage(e, 'PROFILE', 'NAV')}>Profile</button>
             <button type='button' className='mt-2 btn btn-outline-primary' onClick={(e) => this.props.activatePage(e, 'MATCHES', 'NAV')}>Matches</button>
             <button type='button' className='mt-2 btn btn-outline-primary' onClick={(e) => this.props.activatePage(e, 'CREATE_PROFILE', 'NAV')}>Create Profile</button>
           </div>
