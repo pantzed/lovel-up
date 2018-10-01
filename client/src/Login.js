5aaa672992cbfc0be7b83777190abffadcee612a
Jake Lewis <jakeaustinlewis@gmail.com>
2018-10-01 14:11:58 -0500

    redirect user to their profile page

--- client/src/Login.js
+++ client/src/Login.js
@@ -5,8 +5,61 @@ import './Login.css';
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let userData = {};
+    userData.username = this.state.username;
+    userData.password = this.state.password;
+    
+    fetch(`/login`, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify(userData), // body data type must match "Content-Type" header
+    })
+    .then((res) => res.text())
+    .then(text=> JSON.parse(text))
+    .then(data => {
+      console.log(data);
+
+      if(data.error) {
+        let errorMessage = new Error(data.error);
+        return Promise.reject(errorMessage);
+      }
+      else {
+        this.props.activateUser([data]);
+
+        
+      }
+    })
+    .then(()=>this.props.activatePage(null, 'PROFILE', 'LOGIN'))
+    .catch(error => {
+      console.error('error: ', error.message);
+    });
+  }
+
   render() {
     return (
       <div className='row bg-colorful'>
@@ -20,10 +73,10 @@ class Login extends React.Component {
           </div>
           <div className='row d-flex justify-content-center'>
             <div className='col-10 mt-5'>
-              <form action='/users' method='POST'>
+              <form onSubmit={this.handleSubmit}>
                 <div className='form-group'>
-                  <input type='email' className='form-control' placeholder='Username' required/>
-                  <input type='password' className='form-control mt-1' placeholder='Password' required />
+                  <input type='email' className='form-control' placeholder='Username' value={this.state.email} onChange={this.handleChange} name='username' required/>
+                  <input type='password' className='form-control mt-1' placeholder='Password' value={this.state.password} onChange={this.handleChange} name='password' required />
                 </div>
                 <button type='submit' className='btn btn-outline-light full-width shadow-md'>Login</button>
               </form>
@@ -31,7 +84,7 @@ class Login extends React.Component {
           </div>
           <div className='row mt-3 d-flex justify-content-center'>
             <div className='col-10 d-flex justify-content-between'>
-              <span className='text-underline' onClick={(e) => this.props.activatePage(e, 'CREATE_PROFILE', 'NAV')}>Create Account</span>
+              <span className='text-underline' >Create Account</span>
               <span className='text-underline'>Forgot Password?</span>
             </div>
           </div>
--- seeds/0000_users.js
+++ seeds/0000_users.js
@@ -1,14 +1,26 @@
 
+const bcrypt = require('bcrypt');
+let passList = ['pass', 'pass', 'pass', 'pass'];
+let passHash = [];
+for (let i = 0; i<passList.length; i++) {
+  bcrypt.hash(passList[i], 10)
+  .then(hash => {
+    passHash.push(hash);
+  })
+}
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
+
+
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { username: 'ed@gmail.com', password: 'pass', first: 'Ed', last: 'Pantzar', birthdate: '1988-07-20', gender: 'm', seeking_female: true, location: '78741'},
-        { username: 'jake@gmail.com', password: 'pass', first: 'Jake', last: 'Lewis', birthdate: '1992-09-29', gender: 'm', seeking_female: true, location: '78741' },
-        { username: 'amanda@gmail.com', password: 'pass', first: 'Amanda', last: 'Rutherfoord', birthdate: '1987-04-19', gender: 'm', seeking_male: true, location: '78741' },
-        { username: 'louis@gmail.com', password: 'pass', first: 'Louis', last: 'Daily', birthdate: '1990-04-24', gender: 'm', seeking_female: true, location: '78741' }
+        { username: 'ed@gmail.com', password: passHash[0], first: 'Ed', last: 'Pantzar', birthdate: '1988-07-20', gender: 'm', seeking_female: true, location: '78741'},
+        { username: 'jake@gmail.com', password: passHash[0], first: 'Jake', last: 'Lewis', birthdate: '1992-09-29', gender: 'm', seeking_female: true, location: '78741' },
+        { username: 'amanda@gmail.com', password: passHash[0], first: 'Amanda', last: 'Rutherfoord', birthdate: '1987-04-19', gender: 'm', seeking_male: true, location: '78741' },
+        { username: 'louis@gmail.com', password: passHash[0], first: 'Louis', last: 'Daily', birthdate: '1990-04-24', gender: 'm', seeking_female: true, location: '78741' }
       ]);
     });
 };
