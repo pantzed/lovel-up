576f0513f001545a88709fd8d9a3b76ebcae005e
edpantzar@gmail.com <edpantzar@gmail.com>
2018-10-10 09:46:38 -0500

    Removed default seed data

--- seeds/0001_matches.js
+++ seeds/0001_matches.js
@@ -5,9 +5,6 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('matches').insert([
-        {id: 0, user_a: 0, user_b: 1, match_a: true, match_b: true},
-        {id: 1, user_a: 1, user_b: 2, match_a: false, match_b: false},
-        {id: 2, user_a: 2, user_b: 0, match_a: true, match_b: true},
       ]);
     });
 };
